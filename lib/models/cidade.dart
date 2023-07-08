88a571f4ad556c8925c7cdd6e80954403e8a0c25
Fabrício Suhet <fabriciosuhet@hotmail.com>
2023-07-08 20:00:34 -0300

    criacao de map e json decode de cidade

--- lib/models/cidade.dart
+++ lib/models/cidade.dart
@@ -1,10 +1,34 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
 class Cidade {
   int id;
   String nome;
-  
+
   Cidade({
     required this.id,
     required this.nome,
   });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'nome': nome,
+    };
+  }
+
+  factory Cidade.fromMap(Map<String, dynamic> map) {
+    return Cidade(
+      id: map['id'] ?? 0,
+      nome: map['nome'] ?? '',
+    );
+  }
+
+  String toJson() => jsonEncode(toMap());
+
+  factory Cidade.fromJson(String json){
+    Map<String, dynamic> jsonMap = jsonDecode(json);
+    final cidade = Cidade.fromMap(jsonMap);
+    return cidade;
+  }
 }
