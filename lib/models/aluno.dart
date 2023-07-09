8552f401a036fb3eadd68cd5a6b57df7a8f94eb7
Fabrício Suhet <fabriciosuhet@hotmail.com>
2023-07-08 21:05:55 -0300

    criacao do map aluno e json

--- lib/models/aluno.dart
+++ lib/models/aluno.dart
@@ -1,10 +1,12 @@
+import 'dart:convert';
+
 import 'curso.dart';
 import 'endereco.dart';
 
 class Aluno {
   String id;
   String nome;
-  int idade;
+  int? idade;
   List<String> nomeCursos;
   Endereco endereco;
   List<Curso> cursos;
@@ -17,4 +19,36 @@ class Aluno {
     required this.endereco,
     required this.cursos,
   });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'nome': nome,
+      'idade': idade,
+      'nomeCursos': nomeCursos,
+      'endereco': endereco.toMap(),
+      'cursos': cursos.map((curso) => curso.toMap()).toList(),
+    };
+  }
+
+  factory Aluno.fromMap(Map<String, dynamic> map) {
+    return Aluno(
+      id: map['id'] ?? '',
+      nome: map['nome'] ?? '',
+      idade: map['idade'],
+      nomeCursos: map['nomeCursos'],
+      endereco: Endereco.fromMap(map['endereco']),
+      cursos: map['cursos' as List<dynamic>]
+          .map<Curso>((cursoMap) => Curso.fromMap(cursoMap))
+          .toList(),
+    );
+  }
+
+  String toJson() => jsonEncode(toMap());
+
+  factory Aluno.fromJson(String json) {
+    Map<String, dynamic> jsonMap = jsonDecode(json);
+    final aluno = Aluno.fromMap(jsonMap);
+    return aluno;
+  }
 }
